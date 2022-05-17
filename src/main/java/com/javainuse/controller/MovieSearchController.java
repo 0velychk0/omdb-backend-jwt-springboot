a79977012169af4a68e44661de308311f7de4a82
Oleksandr_Velychko2 <Alex.Velichko@gmail.com>
2022-05-17 12:11:19 +0300

    Added MovieSearchController open for usage to any

--- src/main/java/com/javainuse/controller/MovieSearchController.java
+++ src/main/java/com/javainuse/controller/MovieSearchController.java
@@ -1,4 +1,39 @@
 package com.javainuse.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.security.Principal;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/search")
 public class MovieSearchController {
+
+    private static final String OMDBAPI_KEY_NAME = "apikey";
+    private static final String OMDBAPI_KEY_VALUE = "1ac1214b";
+
+    @GetMapping
+    public ResponseEntity getOmdbSearch(@RequestParam Map<String, String> requestParamMap,
+                                        Principal principal) {
+        log.info("getOmdbSearch called with: " + requestParamMap);
+        log.info("getOmdbSearch user auth: " + principal);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.newInstance()
+                .scheme("https").host("www.omdbapi.com").queryParam(OMDBAPI_KEY_NAME, OMDBAPI_KEY_VALUE);
+
+        requestParamMap.forEach(uriComponentsBuilder::queryParam);
+
+        return restTemplate.getForEntity(uriComponentsBuilder.toUriString(), String.class);
+    }
+
 }
